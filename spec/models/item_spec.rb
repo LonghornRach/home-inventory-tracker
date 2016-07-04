0deab08179c0e9035cb92dd7ccc1e3c1c2d45aff
LonghornRach <LonghornRach@users.noreply.github.com>
2016-07-04 15:40:57 -0500

    finished item model tests, all passing

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,17 +2,50 @@ require 'spec_helper'
 
 RSpec.describe Item, type: :model do
   context "validations" do
-    it 'is invalid without a name'
+    it 'is invalid without a name' do
+      expect(Item.create(name: nil)).to_not be_valid
+    end
   end
 
   context "attributes" do
-    it 'has a name'
-    it 'has a room'
-    it 'can have notes'
+
+    before do
+      @item = Item.create(name: "couch")
+
+      @room = Room.create(name: "living")
+    end
+    it 'has a name' do
+      expect(@item.name).to eq("couch")
+    end
+    it 'has a room' do
+      @item.room = @room
+      expect(@item.room.name).to eq("living")
+    end
+    it 'can have notes' do
+      @item.notes = "dingy"
+      expect(@item.notes.empty?).to be_falsey
+    end
   end
 
   context "associations" do
-    it 'belongs to 1 room'
-    it 'has 1 user through room'
+
+    before do
+      @item = Item.create(name: "couch")
+
+      @room = Room.create(name: "living")
+
+      @user = User.create(name: "User Test", email: "user@test.com", password: "user_pass")
+      @user.rooms << @room
+
+      @room.items << @item
+
+    end
+
+    it 'belongs to 1 room' do
+      expect(@item.room).to eq(@room)
+    end
+    it 'has 1 user through room' do
+      expect(@item.room.user).to eq(@user)
+    end
   end
 end
