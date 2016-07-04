68e5a6615ce2f70ee6333310008c08db9c0ca270
LonghornRach <LonghornRach@users.noreply.github.com>
2016-07-04 16:52:07 -0500

    added helpers to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,40 @@ class ApplicationController < Sinatra::Base
     set :session_secret, 'secret'
   end
 
+  helpers do
+    def authorize!(user)
+      if !logged_in?
+        redirect '/'
+        #flash you must log in or sign up
+      else
+        unless current_user == user
+          redirect_home(user)
+          #flash you are not authorized to view that page
+        end
+      end
+    end
+
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find_by_id(session[:id])
+    end
+
+    def login!(user)
+      session[:id] = user.id
+    end
+
+    def redirect_home(user)
+      redirect "/users/#{user.slug}"
+    end
+
+    def logout!
+      session.clear
+      redirect '/'
+      #flash successfully logged out
+    end
+  end
+
 end
